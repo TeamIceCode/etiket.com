811f966ccc97bca4dc879b6ece3b192c9e0958c5
Team Ice Code <132735983+TeamIceCode@users.noreply.github.com>
2023-06-24 12:46:41 +0800

    Update ApiService.jsx link to backend

--- src/Api-service/ApiService.jsx
+++ src/Api-service/ApiService.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiService = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://etiket-be.vercel.app/',
 });
 
 export default apiService;
